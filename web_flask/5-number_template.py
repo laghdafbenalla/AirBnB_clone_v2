6ccffbbf6400f5e7c714b8c6299f0574500a0c65
‘laghdaf <laghdaf.benalla@gmail.com>
2023-10-22 18:13:27 +0100

    Done

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,46 +1,52 @@
 #!/usr/bin/python3
-"""Display flask web application route module"""
-from flask import Flask, render_template
+"""This module starts a flask application with 5 routes."""
 
-app = Flask(__name__)
+from flask import Flask
+# from web_flask import app
+from flask import render_template
+
+app = Flask(__name__,)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """This defines the root route for Hello HBNB!"""
+def index():
+    """Display 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def lone_hbnb():
-    """The defines the /hbnb route for HBNB."""
+def hbnb():
+    """Display 'HBNB'."""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_arg_hbnb(text):
-    """This defines the /c/<text> route"""
-    return "C {}".format(text.replace('_', ' '))
+def c_route(text):
+    """Display 'C' and value of 'text' underscore replaced with space"""
+    return f"C {text.replace('_', ' ')}"
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_arg_hbnb(text):
-    """This display /python/<text> route with text = 'is cool'"""
-    return "Python {}".format(text.replace('_', ' '))
+@app.route('/python', strict_slashes=False)
+def python_route(text='is cool'):
+    """Display 'Python and value of text."""
+    return f"Python {text.replace('_', ' ')}"
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_arg_hbnb(n):
-    """Displays "n is a number" for /number/<n> route"""
-    return "{} is a number".format(n)
+def num_route(n):
+    """Display '<n> is a number' if n is int."""
+    if isinstance(n, int):
+        return f"{n} is a number"
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template_arg_hbnb(n):
-    """This displays 'HTML'for /number_template/<int:n> route"""
-    return render_template('5-number.html', n=n)
+def num_temp_route(n):
+    """Display '<n> is a number' if n is int."""
+    if isinstance(n, int):
+        h1 = f"Number: {n}"
+        return render_template('5-number.html', h1=h1)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
