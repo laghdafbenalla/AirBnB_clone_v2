73e04e41a1366f21cbd89b62fa49fa792821bc68
‘laghdaf <laghdaf.benalla@gmail.com>
2023-10-22 17:41:52 +0100

    5. Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -6,24 +6,20 @@ starts a Flask web application
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
-
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
 
-
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
-
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
